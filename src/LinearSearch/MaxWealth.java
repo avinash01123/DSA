6cb914fa458840cc5b2c5e52afa548486e97e2f4
Cbreaker <avinashlamichhane435@gmail.com>
2024-08-01 16:05:37 +0545

    Richest Customer Wealth

---
+++ src/LinearSearch/MaxWealth.java
@@ -0,0 +1,24 @@
+package LinearSearch;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+
+    }
+    public int maxWealth(int[][] accounts){
+        int ans = Integer.MIN_VALUE;
+        for(int person = 0 ; person<accounts.length; person++){
+            int sum = 0;
+            for (int account= 0; account< accounts[person].length; account++){
+                sum += accounts[person][account];
+;
+            }
+            if (sum > ans){
+                ans = sum;
+            }
+        }
+        return ans;
+
+    }
+}
+
+
