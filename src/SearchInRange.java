a73084dd9b286dbeb936cf41d783c727f34d91da
Cbreaker <avinashlamichhane435@gmail.com>
2024-08-01 09:59:54 +0545

    Search in Range

---
+++ src/SearchInRange.java
@@ -0,0 +1,21 @@
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,77,65,33};
+        int target = 5;
+        int ans = linearSearch(arr, target, 1, 4);
+        System.out.println(ans);
+    }
+    static int linearSearch(int[] arr, int target, int start , int end){
+        if(arr.length == 0){
+            return -1;
+        }
+        for (int i=start; i<=end; i++){
+            int element = arr[i];
+            if(element == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
